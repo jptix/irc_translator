06304b0993623850774e77215ddef5f2171427a1
Markus <markus@towelie.rykroken.net>
2008-06-27 00:42:19 +0200

    config..

--- config.rb
+++ config.rb
@@ -1,7 +1,8 @@
 {
   :host                => 'irc.efnet.no',
   :nick                => 'tolk',
-  :source_channel      => '#jp_tix',
+  :source_channel      => '#mac1',
   :target_channel      => '#mac2',
   :admin_nick          => 'jp_tix',
 }
+
